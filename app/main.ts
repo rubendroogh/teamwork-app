2821f34bc3213d67b86477033bb7b2291e525195
rubendroogh <rubendroogh@hotmail.nl>
2019-04-05 15:01:29 +0200

    Removed unused developer code.

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,11 @@
 import Vue from 'nativescript-vue';
 import Login from './components/Login.vue';
-import VueDevtools from 'nativescript-vue-devtools';
 
 const firebase = require("nativescript-plugin-firebase");
 
 firebase.init({
-  // Optionally pass in properties for database, authentication and cloud messaging,
-  // see their respective docs.
   onAuthStateChanged: function(data) {
-    console.log(data.loggedIn ? "Logged in to firebase" : "Logged out from firebase");
+    Vue.prototype.$loggedIn = data.loggedIn;
   }
 }).then(
   () => {
@@ -19,11 +16,7 @@ firebase.init({
   }
 );
 
-if(TNS_ENV !== 'production') {
-  Vue.use(VueDevtools);
-}
-// Prints Vue logs when --env.production is *NOT* set while building
-Vue.config.silent = (TNS_ENV === 'production');
+Vue.prototype.$firebase = firebase;
 
 new Vue({
   render: h => h('frame', [h(Login)])
