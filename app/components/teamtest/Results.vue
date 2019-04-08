f064e1321024e240d85c75d6e604d9b6ecdc8770
rubendroogh <rubendroogh@hotmail.nl>
2019-04-08 15:17:53 +0200

    Added showing the meaning of scores.

--- app/components/teamtest/Results.vue
+++ app/components/teamtest/Results.vue
@@ -7,8 +7,15 @@
         </ActionBar>
         <StackLayout class="content">
             <StackLayout v-for="(result, index) in results" :key="index" class="results">
-                <Label class="frustration-name" :text="result.text" />
-                <Label :text="result.score" />
+
+                <Label :textWrap="true" class="frustration-name" :text="result.text" />
+                <Label :textWrap="true" v-if="result.score > 7" text="Dit gaat goed zo!" />
+                <Label :textWrap="true" v-if="result.score === 6 || result.score === 7" text="Dit zou potentieel een probleem kunnen vormen! Denk hierom." />
+                <StackLayout v-if="result.score < 6" class="result-container">
+                    <Label text="Dit kan beter aangepakt worden!" />
+                    <Button text="Hoe kan ik hier mee omgaan?" />
+                </StackLayout>
+                
             </StackLayout>
         </StackLayout>
     </Page>
@@ -24,6 +31,9 @@
 
 <style lang="scss" scoped>
     .results{
+        padding: 10;
+        border-bottom: 1 solid #ccc;
+
         .frustration-name{
             font-size: 18;
             font-style: italic;
