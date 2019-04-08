a92f8c58c7af31b2e011832d796e884ec9e9a7b6
rubendroogh <rubendroogh@hotmail.nl>
2019-04-08 14:55:22 +0200

    Added results screen.

--- app/components/teamtest/Results.vue
+++ app/components/teamtest/Results.vue
@@ -1,13 +1,36 @@
 <template>
-    
+    <Page>
+        <ActionBar>
+            <StackLayout orientation="horizontal">
+                <Label text="Team Test" class="page-title" verticalAlignment="center" />
+            </StackLayout>
+        </ActionBar>
+        <StackLayout class="content">
+            <StackLayout v-for="(result, index) in results" :key="index" class="results">
+                <Label class="frustration-name" :text="result.text" />
+                <Label :text="result.score" />
+            </StackLayout>
+        </StackLayout>
+    </Page>
 </template>
 
 <script lang="ts">
     export default{
-        
+        props: {
+            results: Array
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+    .results{
+        .frustration-name{
+            font-size: 18;
+            font-style: italic;
+        }
 
+        Label{
+            text-align: center;
+        }
+    }
 </style>
