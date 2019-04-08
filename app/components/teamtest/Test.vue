4f580146075a3f7a685164a7a1d931b528563c54
rubendroogh <rubendroogh@hotmail.nl>
2019-04-08 11:42:57 +0200

    Added scaffolding for test component.

---
+++ app/components/teamtest/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <Page>
+        <ActionBar>
+            <StackLayout orientation="horizontal">
+                <Label text="Team Test" class="page-title" verticalAlignment="center" />
+            </StackLayout>
+        </ActionBar>
+        <Label text="test" />
+    </Page>
+</template>
+
+<script lang="ts">
+
+    export default{
+        
+    }
+</script>
