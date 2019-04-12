373bbe4a823508bcfbd7a0337e58e4aabe95e1f0
Jeffrey Pieksma <piek1500@student.nhl.nl>
2019-04-12 11:12:55 +0200

    Update

--- app/components/Team/TeamMenu.vue
+++ app/components/Team/TeamMenu.vue
@@ -1,27 +1,23 @@
 <template>
     <Page class="page">
         <ActionBar title="My Team" class="action-bar" />
-
         <GridLayout columns="*,*" rows="*" width="*" height="*" margin="20"
             verticalAlignment="top">
-
             <StackLayout col="0" >
                 <Label text="Deelnemers" class="card"
-                    backgroundColor="red" height="150" @tap="$navigateTo(TeamMembers)"/>
-        
-
-                <Label text="Start de test" class="card"
-                    backgroundColor="green" height="180" @tap="$navigateTo(Test)" />
+                    backgroundColor="#C70039" height="150" @tap="$navigateTo(Members)"/>
+                <Label text="Tests" class="card"
+                    backgroundColor="#19564c" height="180" @tap="$navigateTo(Tests)" />
+                <Label text="Events" class="card"
+                    backgroundColor="#f3814d" height="180" @tap="$navigateTo(Events)" />
             </StackLayout>
-
-            <StackLayout col="1" >
-
+            <StackLayout col="1">
                 <Label text="Safety check" class="card"
-                    backgroundColor="blue" height="180" @tap="$navigateTo(SafetyCheck)" />
-
-                <Label text="Mijn profiel" class="card"
-                    backgroundColor="yellow" height="150" @tap="$navigateTo(MyProfile)" />
-
+                    backgroundColor="#006080" height="180" @tap="$navigateTo(SafetyChecks )" />
+                <Label text="Tips & Tricks" class="card"
+                    backgroundColor="#f7c741" height="150" @tap="$navigateTo(Tips)" />
+                <Label text="links" class="card"
+                    backgroundColor="#77c9f8" height="180" @tap="$navigateTo(Links)" />
             </StackLayout>
         </GridLayout>
     </Page>
@@ -44,5 +40,6 @@
         font-weight: bold;
         border-radius: 10px;
         box-shadow: 1px 1px 1px #333;
+        font-size:16;
     }
 </style>
