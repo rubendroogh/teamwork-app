f487838788b27e5bb77f72ba39c3184ef7624ad4
rubendroogh <rubendroogh@hotmail.nl>
2019-05-03 13:07:36 +0200

    Setting up user service in main.

--- app/classes/UserService.ts
+++ app/classes/UserService.ts
@@ -1,3 +1,5 @@
+// const firebase = require("nativescript-plugin-firebase")
+
 interface IUser {
     uid: string
     name: string
@@ -8,24 +10,32 @@ export default class UserService implements IUser {
     uid: string
     name: string
     teams: Array<string>
+    firebase: any
 
-    constructor(uid: string){
+    constructor(uid: string, firebaseRef: any){
+        this.uid = uid
+        this.firebase = firebaseRef
+
+        
+    }
+
+    public getName(){
+        return this.name
+    }
+
+    public getUid(){
+        return this.uid
+    }
+
+    public getTeams(){
+        return this.teams
+    }
+
+    public setUid(uid: string){
         this.uid = uid
     }
 
-    getName(){
-
-    }
-
-    getUID(){
-
-    }
-
-    getTeams(){
-
-    }
-
-    setName(){
-        
+    public setFirebaseRef(firebase: any){
+        this.firebase = firebase
     }
 }
--- app/main.ts
+++ app/main.ts
@@ -1,22 +1,40 @@
-import Vue from 'nativescript-vue';
-import Login from './components/Login.vue';
+import Vue from 'nativescript-vue'
+import Login from './components/Login.vue'
+
+import UserService from './classes/UserService'
+
+// import CustomActionBar from './components/elements/CustomActionBar.vue'
+
+/* Custom Elements */
+
+// Vue.registerElement(
+//   'CustomActionBar',
+//   () => CustomActionBar
+// )
+
+/* Firebase Authentication */
 
 const firebase = require("nativescript-plugin-firebase");
 
 firebase.init({
-  onAuthStateChanged: function(data) {
-    Vue.prototype.$loggedIn = data.loggedIn;
+  onAuthStateChanged: data => {
+    Vue.prototype.$loggedIn = data.loggedIn
+    
+    if (data.loggedIn){
+      const userService = new UserService(data.user.uid, firebase)
+      Vue.prototype.$user = userService
+    }
   }
 }).then(
   () => {
-    console.log("firebase.init done");
+    console.log("firebase.init done")
   },
   error => {
-    console.log(`firebase.init error: ${error}`);
+    console.log(`firebase.init error: ${error}`)
   }
 );
 
-Vue.prototype.$firebase = firebase;
+Vue.prototype.$firebase = firebase
 
 new Vue({
   render: h => h('frame', [h(Login)])
