9746a402a6a6bf4598966766455fc85a2311895b
rubendroogh <rubendroogh@hotmail.nl>
2019-05-06 15:49:30 +0200

    Added adding team names and members.

---
+++ app/classes/TeamService.ts
@@ -0,0 +1,84 @@
+/** Possible options:
+ *    - name (string): directly set team name
+ *    - members (array of strings): directly set team members
+ */
+interface ITeamOptions {
+    name?: string
+    members?: Array<string>
+}
+
+export default class TeamService{
+    id: string
+    name: string
+    members: Array<string>
+    firebase: any
+    teamDoc: any
+
+    constructor(firebaseRef: any, options?: ITeamOptions){
+        this.firebase = firebaseRef
+
+        let teamData = {
+            name: options.name ? options.name : '',
+            members: []
+        }
+
+        // Create document in firestore
+        let teamCollection = this.firebase.firestore.collection('teams')
+
+        teamCollection.add(teamData).then((doc) => {
+            this.teamDoc = teamCollection.doc(doc.id)
+
+            // Done with the function because extra operations have to be made
+            if(options.members) {
+                this.addMembers(options.members)
+            }
+        })
+    }
+
+    public getName(){
+        return this.name
+    }
+
+    public getMembers(){
+        return this.members
+    }
+
+    public setName(name: string){
+        this.name = name
+
+        try {
+            this.teamDoc.update({
+                name: this.name
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    // TODO: add check for correct UID
+    public addMembers(uids: Array<string>) {
+        // Create array of correctly formatted uids
+        let newMembers = uids.map(uid => `/users/${uid}`)
+
+        // Join members array
+        this.members = (this.members) ? this.members.concat(newMembers) : newMembers
+
+        // Update data in Firestore
+        this.teamDoc.update({
+            members: this.members
+        }).then(() => {
+            return this.members
+        })
+    }
+
+    // TODO: add check for correct UID
+    public addMember(uid: string){
+        this.members.push(`/users/${uid}`)
+
+        this.teamDoc.update({
+            members: this.members
+        }).then(() => {
+            return this.members
+        })
+    }
+}
