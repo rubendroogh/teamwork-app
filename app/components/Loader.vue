750f1bf2ea06a941f4bef3526264e7cfc6e9d584
rubendroogh <rubendroogh@hotmail.nl>
2019-05-08 23:12:43 +0200

    Added transition and history clear to loader nav.

--- app/components/Loader.vue
+++ app/components/Loader.vue
@@ -1,6 +1,8 @@
 <template>
     <Page :actionBarHidden="true" androidStatusBarBackground="#008FEF" >
         <Gradient direction="to bottom" colors="#008FEF, #025D9A" >
+            <ActivityIndicator busy="true" margin="auto" marginTop="42%" color="#FDFDFD" />
+            <Label class="title" color="#FDFDFD" marginTop="10" text="Laden..." />
         </Gradient>
     </Page>
 </template>
@@ -12,13 +14,22 @@
     export default {
         created(){
             this.$firebase.getCurrentUser().then(user => {
-                this.$userService.loadWithUid(user.uid).then(currentUser => {
-                    this.$navigateTo(TeamMenu)
+                this.$userService.loadWithUid(user.uid).then(() => {
+                    this.$navigateTo(TeamMenu, {
+                        clearHistory: true,
+                        transitionAndroid: {
+                            name: "slide"
+                        },
+                    })
                 })
-                
             }).catch(error => {
                 console.log(`Error: ${error}`)
-                this.$navigateTo(Login)
+                this.$navigateTo(Login, {
+                    clearHistory: true,
+                    transitionAndroid: {
+                        name: "slide"
+                    },
+                })
             })
         }
     }
