1fbdc44e873a68b69a151643521e3f2f89deb38f
rubendroogh <rubendroogh@hotmail.nl>
2019-05-09 16:39:04 +0200

    Added empty name check and layout createTeam.

--- app/components/teamAdmin/CreateTeam.vue
+++ app/components/teamAdmin/CreateTeam.vue
@@ -1,15 +1,18 @@
 <template>
     <Page>
-        <CustomActionBar title="Team aanmaken" />
-        <GridLayout class="container" columns="*" rows="*, auto, *, auto *, auto">
-            <TextField v-model="teamName" hint="Team naam" row="1" />
-            <AddMembers v-on:members="updateMembers($event)" row="3" />
-            <Button text="Team aanmaken" @tap="createTeam()" row="5" />
+        <CustomActionBar title="Team aanmaken"/>
+        <GridLayout class="container" columns="*" rows="auto, auto, *, auto">
+            <Label class="title" text="Vul naam in" row="0"/>
+            <TextField v-model="teamName" hint="Team naam" row="1"/>
+            <AddMembers v-on:members="updateMembers($event)" row="2"/>
+            <Button text="Team aanmaken" @tap="createTeam()" row="3"/>
         </GridLayout>
     </Page>
 </template>
 
 <script lang="ts">
+    const dialogs = require("tns-core-modules/ui/dialogs")
+
     import TeamMenu from '../TeamMenu.vue'
 
     import TeamService from '../../classes/TeamService'
@@ -21,16 +24,21 @@
                 members: []
             }
         },
-        mounted(){
-            
-        },
         methods: {
             createTeam(){
-                let team = new TeamService(this.$firebase, {
-                    name: this.teamName,
-                    members: this.members
-                })
-                this.$navigateTo(TeamMenu)
+                if(this.teamName === '') {
+                    dialogs.alert({
+                        title: "Let op!",
+                        message: "Je hebt nog geen naam ingevuld. Een team heeft een naam nodig.",
+                        okButtonText: "Ok"
+                    })
+                } else {
+                    let team = new TeamService(this.$firebase, {
+                        name: this.teamName,
+                        members: this.members
+                    })
+                    this.$navigateTo(TeamMenu)
+                }
             },
             updateMembers(event){
                 this.members = event
