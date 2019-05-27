44ddffea6eb3ef8be7638b8bcd0b4ae83e2c6d64
rubendroogh <rubendroogh@hotmail.nl>
2019-05-27 15:04:40 +0200

    Added FAB to SC menu.

--- app/components/elements/CardList.vue
+++ app/components/elements/CardList.vue
@@ -1,7 +1,7 @@
 <template>
     <ScrollView>
         <FlexboxLayout flexWrap="wrap">
-            <StackLayout class="card" width="46%" v-for="(item, key) in items" :key="`item-${key}`">
+            <StackLayout class="card" width="47%" v-for="(item, key) in items" :key="`item-${key}`">
                 <Label :text="item.title" :textWrap="true"/>
             </StackLayout>
         </FlexboxLayout>
@@ -9,23 +9,10 @@
 </template>
 
 <script>
-    import { screen } from 'tns-core-modules/platform'
-
     export default {
         props: {
             items: {
                 type: Object
-            }
-        },
-        data() {
-            return {
-                screenWidth: screen.mainScreen.widthPixels
-            }
-        },
-        methods: {
-            calcWidth() {
-                console.log(screen.mainScreen.widthPixels)
-                return (screen.mainScreen.widthPixels / 2) - 20
             }
         }
     }
--- app/components/safetyCheck/SafetyCheckMenu.vue
+++ app/components/safetyCheck/SafetyCheckMenu.vue
@@ -1,10 +1,17 @@
 <template>
     <Page>
         <CustomActionBar title="Safety check" />
-        <StackLayout class="container-fluid">
-            <Banner mode="light" text="Huidige safety check" image="https://images.pexels.com/photos/53594/blue-clouds-day-fluffy-53594.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/>
-            <CardList :items="prevSafetyChecks" />
-        </StackLayout>
+        <GridLayout class="container-fluid" rows="auto, *, auto">
+            <Banner row="0" mode="light" text="Huidige safety check" image="https://images.pexels.com/photos/53594/blue-clouds-day-fluffy-53594.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/>
+            <CardList row="1" rowSpan="2" :items="prevSafetyChecks" />
+            <fab
+                @tap="$navigateTo()"
+                row="2"
+                icon="~/assets/images/add-icon.png"
+                rippleColor="#f1f1f1"
+                class="fab-button"
+            ></fab>
+        </GridLayout>
     </Page>
 </template>
 
