b1005a9cd8337d4e3c4c70b94507588e7637fabc
rubendroogh <rubendroogh@hotmail.nl>
2019-05-28 18:02:39 +0200

    Styling changes.

--- app/components/safetyCheck/SafetyCheckMenu.vue
+++ app/components/safetyCheck/SafetyCheckMenu.vue
@@ -1,17 +1,20 @@
 <template>
     <Page>
         <CustomActionBar title="Safety check" />
-        <GridLayout class="container-fluid" rows="auto, *, auto">
-            <Banner row="0" mode="light" text="Huidige safety check" image="https://images.pexels.com/photos/53594/blue-clouds-day-fluffy-53594.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/>
-            <CardList row="1" rowSpan="2" :items="prevSafetyChecks" />
-            <fab
-                @tap="$navigateTo(safetyCheckIntroComponent)"
-                row="2"
-                icon="~/assets/images/add-icon.png"
-                rippleColor="#f1f1f1"
-                class="fab-button"
-            ></fab>
-        </GridLayout>
+        <StackLayout>
+            <Banner mode="light" text="Huidige safety check" image="https://images.pexels.com/photos/53594/blue-clouds-day-fluffy-53594.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/>
+            <GridLayout rows="auto, *, auto" class="container">
+                <Label row="0" text="Uitslagen vorige safety checks" class="subtitle mt-2"/>
+                <CardList row="1" rowSpan="2" :items="prevSafetyChecks" />
+                <fab
+                    @tap="$navigateTo(safetyCheckIntroComponent)"
+                    row="2"
+                    icon="~/assets/images/add-icon.png"
+                    rippleColor="#f1f1f1"
+                    class="fab-button"
+                ></fab>
+            </GridLayout>
+        </StackLayout>
     </Page>
 </template>
 
