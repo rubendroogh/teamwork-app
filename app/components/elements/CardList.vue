6621787dbc5aaa7ed09080b08e797960909bd016
rubendroogh <rubendroogh@hotmail.nl>
2019-06-03 18:24:08 +0200

    Removed card margin.

--- app/components/elements/CardList.vue
+++ app/components/elements/CardList.vue
@@ -2,7 +2,7 @@
     <ScrollView>
         <FlexboxLayout flexWrap="wrap">
             <GridLayout rows="auto, auto, *" columns="auto, *" class="card" width="47%" v-for="(item, key) in items" :key="`item-${key}`">
-                <Label row="0" :text="item.title" class="bold mb-1" :textWrap="true"/>
+                <Label row="0" :text="item.title" class="bold" :textWrap="true"/>
                 <Label row="1" :text="item.subTitle || ''" :textWrap="true"/>
                 <Label col="1" rowSpan="3" :text="item.extra || ''" class="subtitle text-right vertical-center bold"/>
             </GridLayout>
