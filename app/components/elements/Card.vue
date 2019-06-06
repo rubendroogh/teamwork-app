58b1a699dd7fedf808d3771a1ea514ddefd0cbf9
rubendroogh <rubendroogh@hotmail.nl>
2019-06-06 16:01:09 +0200

    Added height prop in card.

--- app/components/elements/Card.vue
+++ app/components/elements/Card.vue
@@ -1,5 +1,5 @@
 <template>
-    <GridLayout rows="auto, auto, *" columns="auto, *" class="card" :width="width">
+    <GridLayout rows="auto, auto, *" columns="auto, *" class="card" :width="width" :height="height">
         <Label row="0" :text="item.title" class="bold" :textWrap="true"/>
         <Label row="1" :text="item.subTitle || ''" :textWrap="true"/>
         <Label col="1" rowSpan="3" :text="item.extra || ''" class="subtitle text-right vertical-center bold"/>
@@ -14,7 +14,8 @@
             },
             width: {
                 default: "100%"
-            }
+            },
+            height: {}
         }
     }
 </script>
