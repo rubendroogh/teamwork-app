5e20adecd939fa006703dd9c612b33c16410eb9e
rubendroogh <rubendroogh@hotmail.nl>
2019-06-19 21:21:10 +0200

    Single meeting layout.

--- app/components/meetings/SingleMeeting.vue
+++ app/components/meetings/SingleMeeting.vue
@@ -1,12 +1,25 @@
 <template>
     <Page>
         <CustomActionBar title="Vergadering inzien"/>
-        <Banner :title="meeting.subject" subtitle="Georganiseerd door Ruben" mode="light" image="https://images.pexels.com/photos/1068523/pexels-photo-1068523.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/>
-        <StackLayout class="container">
-            <Label class="title" text="Agenda punten"/>
-            <Label class="title" text="Voorzitter en notulist"/>
-            <Label :text="'Voorzitter: ' + meeting.leader"/>
-            <Label :text="'Notulist: ' + meeting.secretary"/>
+        <StackLayout class="container-fluid">
+            <Banner
+                :title="meeting.subject"
+                subtitle="Georganiseerd door Ruben"
+                mode="dark"
+                image="https://images.pexels.com/photos/926987/pexels-photo-926987.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            />
+            <StackLayout class="container">
+                <Label class="m-1" :text="meeting.startTime | dateTimeFormat"/>
+                <Label class="title mb-1" text="Agenda punten"/>
+                <ListView for="item in meeting.agendaItems" height="180">
+                    <v-template>
+                        <Label class="list-item" :text="item.description"/>
+                    </v-template>
+                </ListView>
+                <Label class="title mb-1" text="Informatie"/>
+                <Label class="ml-1" :text="'Voorzitter: ' + meeting.leader"/>
+                <Label class="ml-1" :text="'Notulist: ' + meeting.secretary"/>
+            </StackLayout>
         </StackLayout>
     </Page>
 </template>
